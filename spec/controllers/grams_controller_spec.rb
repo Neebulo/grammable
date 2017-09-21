44c29d71afc46b35c52a6602c32b86c045865edb
Cris <rhapsodyshadow@yahoo.com>
2017-09-20 19:08:46 -0700

    initial value message

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#update action" do
     it "should allow users to successfully update grams" do
+      gram = FactoryGirl.create(:gram, message: "Initial Value")
 
     end
 
